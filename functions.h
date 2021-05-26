35cc3f691f6169b81bdc355cf8ad02dc252f254a
Venkateshprasad3 <42870395+Venkateshprasad3@users.noreply.github.com>
2021-05-26 19:27:38 +0530

    Update functions.h

--- functions.h
+++ functions.h
@@ -2,31 +2,109 @@
 #include <fstream>
 #include<string.h>
 #include <ctime>
+#include<sstream> 
 using namespace std;
 
 #ifndef FUNCTIONS_H    
 #define FUNCTIONS_H
 
 
- void bg_finder(int i)
+
+
+int getFields()
 {
+	int field;
+	cout<<"Enter Field";
+	char fields[10][100]={ "\n1. Id\n", "2. First Name\n", "3. Last Name\n","4. Age\n","5. Status \n",
+							"6. Blood Group\n","7. State\n","8. City\n","9. Mobile Number\n","10. Address\n"			};
+	for(int i=0;i<10;i++)
+	{
+		cout<<fields[i];
+	}
 	
-char blo[15][15] ={"O+ve","O-ve","A+ve","A-ve","B+ve","B-ve","AB+ve","AB-ve"};
+	cin>>field;
+	return field;
+}
+
+int typeSearch()
+{
+	int field;
+	cout<<"Enter Type";
+	char fields[10][100]={ "\n1. At Begining\n", "2.Contains\n", "3.At End\n","4.Equals\n"	};
+	for(int i=0;i<4;i++)
+	{
+		cout<<fields[i];
+	}
+	
+	cin>>field;
+	return field;
+}
+
+
+
+string return_bg(int i)
+{
+	string str;
+	char blo[15][15] ={"A+ve","A-ve","AB+ve","AB-ve","B+ve","B-ve","O+ve","O-ve"};
+	for(int j=0;blo[i-1][j]!='\0';j++)
+	{
+		str+=blo[i-1][j];
+	}
+	
+	return str;
+}
+
+void bg_finder(int i)
+{
+	string str;
+char blo[15][15] ={"A+ve","A-ve","AB+ve","AB-ve","B+ve","B-ve","O+ve","O-ve"};
+
 cout<<blo[i-1];
  
 }
+
+
 
 
 
  void p_finder(int i)
 {
 	
-char blo[15][100] ={"General Checkup","Serious illness","Accident","Minor illness","Others"};
+char blo[15][100] ={"Accident","General Checkup","Minor illness","Others","Serious illness"};
 cout<<blo[i-1];
  
 }
 
+
+
+string toString(char *s)
+{
+	string str;
+	int size=0;
+	for(int i=0;s[i]!='\0';i++)
+	{
+		size++;
+	}
+	for(int i=0;i<size;i++)
+	{
+		str=str+s[i];
+	}
+	//cout<<str;
+	return str;
+}
+
 char* upper_case(char *s)//converts character array to upper case
+{
+	
+    for(int i=0;s[i]!='\0';i++)
+    {
+        s[i]=toupper(s[i]);
+    }
+   
+    return s;
+}
+
+string upper_case(string s)//converts character array to upper case
 {
 	
     for(int i=0;s[i]!='\0';i++)
@@ -50,14 +128,14 @@ int blood()  //getter
 		}
 	flag=1;
 	cout<<"Enter Blood group";
-	cout<<endl<<"1. O+ve"<<endl;
-	cout<<"2. O-ve"<<endl;
-	cout<<"3. A+ve"<<endl;
-	cout<<"4. A-ve"<<endl;
+	cout<<endl<<"1. A+ve"<<endl;
+	cout<<"2. A-ve"<<endl;
+	cout<<"3. AB+ve"<<endl;
+	cout<<"4. AB-ve"<<endl;
 	cout<<"5. B+ve"<<endl;
 	cout<<"6. B-ve"<<endl;
-	cout<<"7. AB+ve"<<endl;
-	cout<<"8. AB-ve"<<endl;
+	cout<<"7. O+ve"<<endl;
+	cout<<"8. O-ve"<<endl;
 	cin>>bg;
 }while(bg>8);
 	return bg;
@@ -77,11 +155,13 @@ int dept()  //getter
 		}
 	flag=1;
 	cout<<"Purpose of Visit ";
-	cout<<endl<<"1. General checkup"<<endl;
-	cout<<"2. Serious illness"<<endl;
-	cout<<"3. Accident"<<endl;
-	cout<<"4. Minor illness"<<endl;
-	cout<<"5. Others"<<endl;
+	
+	
+	cout<<endl<<"1. Accident"<<endl;
+	cout<<"2. General checkup"<<endl;
+	cout<<"3. Minor illness"<<endl;
+	cout<<"4. Others"<<endl;
+	cout<<"5. Serious illness"<<endl;
 	
 	cin>>bg;
 }while(bg>5);
